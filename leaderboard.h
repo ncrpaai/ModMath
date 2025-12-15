838f86122d12f7c7488c5b40abdce0b77bce9ad0
ncrpaai <154041477+ncrpaai@users.noreply.github.com>
2025-12-15 15:07:10 +0700

    Update leaderboard.h

--- leaderboard.h
+++ leaderboard.h
@@ -1,22 +1,26 @@
+// Name : Nattaya Seangon
+// ID : 68070503420
+
 #ifndef LEADERBOARD_H
 #define LEADERBOARD_H
 
 #define MAX_USERS 100
 
-/* Structure to hold user score data */
+// Structure to hold user score data 
 typedef struct {
     char username[50];
     int totalScore;
     int totalAttempts;
 } UserScore;
 
-/* Display the leaderboard */
+// Display the leaderboard 
 void displayLeaderboard();
 
-/* Calculate total scores for all users */
+// Calculate total scores for all users 
 int calculateTotalScores(UserScore users[], int maxUsers);
 
-/* Sort users by total score (descending) */
+// Sort users by total score (descending)
 void sortUsersByScore(UserScore users[], int count);
+
 
 #endif
